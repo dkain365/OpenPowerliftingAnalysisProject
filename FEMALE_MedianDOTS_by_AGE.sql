91ab4ae0ec711230322bf41d968ecac71e339ff7
Dennis K <127635639+dkain365@users.noreply.github.com>
2023-09-05 17:27:27 -0400

    Add files via upload
    Male and Female Median DOTS queries by age

---
+++ FEMALE_MedianDOTS_by_AGE.sql
@@ -0,0 +1,31 @@
+SELECT
+    female_age_range,
+    ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dots) AS NUMERIC), 2) AS median_dots
+FROM (
+    SELECT
+        sex,
+        age,
+        dots,
+        CASE
+            WHEN age BETWEEN 19 AND 20 THEN '19-20'
+            WHEN age BETWEEN 20 AND 21 THEN '20-21'
+            WHEN age BETWEEN 21 AND 22 THEN '21-22'
+            WHEN age BETWEEN 22 AND 23 THEN '22-23'
+            WHEN age BETWEEN 23 AND 24 THEN '23-24'
+            WHEN age BETWEEN 24 AND 25 THEN '24-25'
+            WHEN age BETWEEN 25 AND 26 THEN '25-26'
+            WHEN age BETWEEN 26 AND 27 THEN '26-27'
+            WHEN age BETWEEN 27 AND 28 THEN '27-28'
+            WHEN age BETWEEN 28 AND 29 THEN '28-29'
+            WHEN age BETWEEN 29 AND 30 THEN '29-30'
+            ELSE 'Over 30'
+        END AS female_age_range
+    FROM
+        openpowerlifting_data
+    WHERE
+        sex = 'F'
+) AS age_data
+GROUP BY
+    female_age_range
+ORDER BY
+    female_age_range;
---
+++ male_MEDIAN_DOTS_30to40.sql
@@ -0,0 +1,35 @@
+SELECT
+    male_age_range,
+    ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dots) AS NUMERIC), 2) AS median_dots
+FROM (
+    SELECT
+        sex,
+        age,
+        dots,
+        CASE
+            WHEN age BETWEEN 29 AND 30 THEN '29-30'
+            WHEN age BETWEEN 30 AND 31 THEN '30-31'
+            WHEN age BETWEEN 31 AND 32 THEN '31-32'
+            WHEN age BETWEEN 32 AND 33 THEN '32-33'
+            WHEN age BETWEEN 33 AND 34 THEN '33-34'
+            WHEN age BETWEEN 34 AND 35 THEN '34-35'
+            WHEN age BETWEEN 35 AND 36 THEN '35-36'
+            WHEN age BETWEEN 36 AND 37 THEN '36-37'
+            WHEN age BETWEEN 37 AND 38 THEN '37-38'
+            WHEN age BETWEEN 38 AND 39 THEN '38-39'
+            WHEN age BETWEEN 39 AND 40 THEN '39-40'
+            ELSE 'Other'
+        END AS male_age_range
+    FROM
+        openpowerlifting_data
+    WHERE
+        sex = 'M'
+) AS age_data
+GROUP BY
+    male_age_range
+ORDER BY
+    male_age_range;
+
+
+
+
